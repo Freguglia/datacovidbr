66b34cf8e09bbd9dc72da0f1d9eb6267631997b5
Freguglia <victorfreguglia@gmail.com>
2020-04-02 16:37:42 -0300

    Added working functions and initial documentation.

---
+++ R/CSSEGISandData.R
@@ -0,0 +1,50 @@
+#' @name CSSEGISandData
+#' @title Data from the `CSSEGISandData/COVID-19` repository
+#' 
+#' @description Extracts updated data from the repository by Johns Hopkins CSSE
+#' (https://github.com/CSSEGISandData/COVID-19)
+#' by download the file in `.csv` format available and loading it into a comprehensive
+#' `tibble` object.
+#' 
+#' @inheritParams brMinisterioSaude
+#' 
+#' @return A `tibble` object.
+#' 
+#' @importFrom dplyr summarise group_by left_join
+#' @importFrom magrittr %>%
+#' @importFrom tidyr starts_with
+#' @export
+CSSEGISandData <- function(silent = FALSE){
+  df_cases <- read.csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+  df_cases <- tidyr::pivot_longer(df_cases, cols = starts_with("X"),
+                                  names_to = "data", values_to = "casosAcumulados")
+  df_cases$data <- gsub("X", df_cases$data, replacement = "")
+  df_cases$data <- lubridate::as_date(as.character(df_cases$data),
+                                      format = "%m.%d.%y", tz = "UTC")
+  df_cases <- df_cases %>% group_by(Country.Region, data) %>%
+    summarise(casosAcumulados = sum(casosAcumulados, na.rm = TRUE))
+  
+  df_deaths <- read.csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+  df_deaths <- tidyr::pivot_longer(df_deaths, cols = starts_with("X"),
+                                   names_to = "data", values_to = "obitosAcumulado")
+  df_deaths$data <- gsub("X", df_deaths$data, replacement = "")
+  df_deaths$data <- lubridate::as_date(as.character(df_deaths$data),
+                                       format = "%m.%d.%y", tz = "UTC")
+  df_deaths <- df_deaths %>% group_by(Country.Region, data) %>%
+    summarise(obitosAcumulados = sum(obitosAcumulado, na.rm = TRUE))
+  
+  df_recovered <- read.csv("https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+  df_recovered <- tidyr::pivot_longer(df_recovered, cols = starts_with("X"),
+                                      names_to = "data", values_to = "recuperadosAcumulado")
+  df_recovered$data <- gsub("X", df_recovered$data, replacement = "")
+  df_recovered$data <- lubridate::as_date(as.character(df_recovered$data),
+                                          format = "%m.%d.%y", tz = "UTC")
+  df_recovered <- df_recovered %>% group_by(Country.Region, data) %>%
+    summarise(recuperadosAcumulados = sum(recuperadosAcumulado, na.rm = TRUE))
+  
+  if(!silent) cat("Latest Update: ", as.character(max(df_recovered$data)), "\n")
+  
+  return(df_cases %>%
+           left_join(df_deaths, by = c("Country.Region", "data")) %>%
+           left_join(df_recovered, by = c("Country.Region", "data")))
+}
---
+++ R/br_ministerio_saude.R
@@ -0,0 +1,23 @@
+#' @name brMinisterioSaude
+#' @title Brazilian Health Ministry COVID data
+#' 
+#' @description Extracts updated data from the page of Brazilian Health Ministry 
+#' (https://covid.saude.gov.br/)
+#' by download the file in `.csv` format available and loading it into a comprehensive
+#' `tibble` object.
+#' 
+#' @param silent `logical` indicating if messages should be hidden.
+#' 
+#' @return A `tibble` object.
+#' 
+#' @importFrom glue glue
+#' @export
+brMinisterioSaude <- function(silent = FALSE){
+  tm <- lubridate::now(tzone = "Brazil/East") - lubridate::days(1)
+  dt <- format.Date(tm, format = "%Y%m%d")
+  df <- read.csv(glue("https://covid.saude.gov.br/assets/files/COVID19_", 
+                      dt, ".csv"), sep = ";")
+  df$data <- lubridate::as_date(as.character(df$data), format = "%d/%m/%y", tz = "Brazil/East")
+  if(!silent) cat("Latest Update: ", as.character(max(df$data)), "\n")
+  return(tibble::as_tibble(df))
+}
