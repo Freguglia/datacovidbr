bdbf178a62475476717158e01cac44f83617412d
Freguglia <victorfreguglia@gmail.com>
2020-04-03 11:12:49 -0300

    Using tryCatch to prevent possible problems when data is still not updated.

--- R/ministeriosaude.R
+++ R/ministeriosaude.R
@@ -13,10 +13,19 @@
 #' @importFrom glue glue
 #' @export
 brMinisterioSaude <- function(silent = !interactive()){
-  tm <- lubridate::now(tzone = "Brazil/East") - lubridate::days(1)
-  dt <- format.Date(tm, format = "%Y%m%d")
-  df <- read.csv(glue("https://covid.saude.gov.br/assets/files/COVID19_{dt}.csv"), 
-                 sep = ";", stringsAsFactors = FALSE)
+  shift <- 0
+  df <- "not_found"
+  while(shift < 3 & !is(df, "data.frame")){
+    tm <- lubridate::now(tzone = "Brazil/East") - lubridate::days(shift)
+    dt <- format.Date(tm, format = "%Y%m%d")
+    df <- tryCatch({
+      suppressWarnings(
+        read.csv(glue("https://covid.saude.gov.br/assets/files/COVID19_{dt}.csv"), 
+                 sep = ";", stringsAsFactors = FALSE))
+    }, error = function(er) "not_found")
+    shift <- shift + 1
+  }
+  if(!is(df, "data.frame")) stop("Unable to download file.")
   df$data <- lubridate::as_date(as.character(df$data), format = "%d/%m/%y", tz = "Brazil/East")
   if(!silent) cat("Latest Update: ", as.character(max(df$data)), "\n")
   return(tibble::as_tibble(df))
