f492e93f2ee4bf7baa78da33f3fb88b5752b6599
Freguglia <victorfreguglia@gmail.com>
2020-04-02 21:18:02 -0300

    Split tests.

--- tests/testthat/test-everything_works.R
+++ tests/testthat/test-everything_works.R
@@ -1,4 +1,11 @@
-test_that("multiplication works", {
-  expect_is(brMinisterioSaude(silent = TRUE), "tbl_df")
-  expect_is(CSSEGISandData(silent = TRUE), "tbl_df")
+test_that("Ministério da Saúde", {
+  expect_is(brMinisterioSaude(), "tbl_df")
+})
+
+test_that("JHU", {
+  expect_is(CSSEGISandData(), "tbl_df")
+})
+
+test_that("Ministério da Saúde", {
+  expect_is(brasilio(), "tbl_df")
 })
