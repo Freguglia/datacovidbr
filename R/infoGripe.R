f6b6cd5f6c895b3fd74eea5e29456a3a429a5988
Freguglia <victorfreguglia@gmail.com>
2020-04-21 08:09:16 -0300

    docs for infoGripe()

--- R/infoGripe.R
+++ R/infoGripe.R
@@ -1,13 +1,25 @@
-#' @param state `character` vector com UFs inclusos.
+#' @name infoGripe
+#' @title Data from http://info.gripe.fiocruz.br/
+#' 
+#' @description Extracts the data from http://info.gripe.fiocruz.br/ displayed
+#' when the databases `SRAGCOVID` and `OBITOCOVID` are selected.
+#' 
+#' @param state `character` vector with UFs to include.
+#' 
+#' @return A `list` with elements `casos` e `obitos`, containing `tibbles`
+#' with the distribution of cases and deaths, respectively, by week and
+#' age.
+#' 
+#' @importFrom utils URLencode
 #' @export
 infoGripe <- function(state = NULL){
   casos <- data.frame()
   if(is.null(state)) {
-    state <- c("Alagoas", "Amapá", "Bahia", "Ceará", "Distrito Federal", 
-               "Espírito Santo", "Goiás", "Maranhão", "Minas Gerais", "Mato Grosso do Sul",
-               "Mato Grosso", "Pará", "Paraíba", "Pernambuco", "Piauí", "Paraná", "Rio de Janeiro",
-               "Rio Grande do Norte", "Rondônia", "Roraima", "Rio Grande do Sul", "Santa Catarina",
-               "Sergipe", "São Paulo", "Tocantins")
+    state <- c("Alagoas", "Amap\u00e1", "Bahia", "Cear\u00e1", "Distrito Federal", 
+               "Esp\u003drito Santo", "Goiu00e1s", "Maranh\u00e3o", "Minas Gerais", "Mato Grosso do Sul",
+               "Mato Grosso", "Par\u00e1", "Para\u003dba", "Pernambuco", "Piau\u003d", "Paran\u00e1", "Rio de Janeiro",
+               "Rio Grande do Norte", "Rond\u00f4nia", "Roraima", "Rio Grande do Sul", "Santa Catarina",
+               "Sergipe", "S\u00e3o Paulo", "Tocantins")
   }
   for(i in state){
     if(interactive()) { cat("\r Loading", i, "                    ") }
