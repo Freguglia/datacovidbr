5edfae9d9dc309500f79c64a70cd2cd64466f9b8
Freguglia <victorfreguglia@gmail.com>
2020-05-07 16:50:32 -0300

    Added registro_civil_diario().

--- R/registro_civil.R
+++ R/registro_civil.R
@@ -8,7 +8,7 @@
 #' @return Um `tibble` com as mortes por gênero, faixa etária e causa nos anos de 
 #' 2019 e 2020, do dia 16 de março até a data atual.
 #' 
-#' @importFrom dplyr bind_rows
+#' @importFrom dplyr bind_rows select
 #' @importFrom tidyr pivot_longer
 #' @importFrom httr GET content
 #' @importFrom glue glue
@@ -32,4 +32,23 @@ registro_civil <- function(state = "all", city_id = "all"){
   out <- rbind(out, df)
   }
   return(as_tibble(out))
+}
+
+#' @rdname registro_civil
+#' 
+#' @export
+registro_civil_diario <- function(state = "all", city_id = "all"){
+  if(length(city_id) > 1){stop("Um 'city_id' por vez, ou 'all' para todas as cidades to estado.")}
+  if(length(state) > 1){stop("Um 'state' por vez, ou 'all' para o brasil inteiro.")}
+  api_url <- glue::glue("https://transparencia.registrocivil.org.br/api/covid-covid-registral?start_date=2020-03-16&end_date={today()}&state={state}&city_id={city_id}&places[]=HOSPITAL&places[]=DOMICILIO&places[]=VIA_PUBLICA&places[]=AMBULANCIA&places[]=OUTROS&chart=chart5")
+  a <- GET(api_url) %>% content() %>% "[["("chart")
+  df <- lapply(a, function(date){
+    lapply(date, as.data.frame) %>%
+      bind_rows(.id = "causa")
+  }) %>% 
+    bind_rows(.id = "dia") %>%
+    select(-data_obito, -tipo_doenca, -dia_obito)
+  df$dia <- as.Date(strftime(df$dia, format = "%d-%m-%y"))
+  df$total <- as.numeric(df$total)
+  return(as_tibble(df))
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 read.csv <- utils::read.csv
 globalVariables(c("Country.Region", "data", "casosAcumulados", "obitosAcumulado",
-                  "recuperadosAcumulado", "Province.State", "Lat", "Long", "ano", "idade"))
+                  "recuperadosAcumulado", "Province.State", "Lat", "Long", "ano", "idade",
+                  "data_obito", "tipo_doenca", "dia_obito"))
