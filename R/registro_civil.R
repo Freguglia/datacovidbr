f3abcd5a27efc5734a9b93965ad1642a159dea92
Freguglia <victorfreguglia@gmail.com>
2020-05-14 16:59:12 -0300

    Modified user-agent for registro_civil() calls.

--- R/registro_civil.R
+++ R/registro_civil.R
@@ -10,7 +10,7 @@
 #' 
 #' @importFrom dplyr bind_rows select
 #' @importFrom tidyr pivot_longer
-#' @importFrom httr GET content
+#' @importFrom httr GET content add_headers
 #' @importFrom glue glue
 #' @importFrom lubridate today
 #' @importFrom tibble as_tibble
@@ -22,7 +22,7 @@ registro_civil <- function(state = "all", city_id = "all"){
   out <- data.frame()
   for(gender in c("M", "F")){
   api_url <- glue::glue("https://transparencia.registrocivil.org.br/api/covid-covid-registral?start_date=2020-03-16&end_date={lubridate::today()-1}&state={state}&city_id={city_id}&chart=chart2&gender={gender}")
-  a <- GET(api_url) %>% content() %>% "[["("chart")
+  a <- GET(api_url, add_headers("user-agent" = "Mozilla/5.0")) %>% content() %>% "[["("chart")
   df <- lapply(a, function(age){
     lapply(age, as.data.frame) %>%
       bind_rows(.id = "ano")
@@ -41,7 +41,7 @@ registro_civil_diario <- function(state = "all", city_id = "all"){
   if(length(city_id) > 1){stop("Um 'city_id' por vez, ou 'all' para todas as cidades to estado.")}
   if(length(state) > 1){stop("Um 'state' por vez, ou 'all' para o brasil inteiro.")}
   api_url <- glue::glue("https://transparencia.registrocivil.org.br/api/covid-covid-registral?start_date=2020-03-16&end_date={today()}&state={state}&city_id={city_id}&places[]=HOSPITAL&places[]=DOMICILIO&places[]=VIA_PUBLICA&places[]=AMBULANCIA&places[]=OUTROS&chart=chart5")
-  a <- GET(api_url) %>% content() %>% "[["("chart")
+  a <- GET(api_url, add_headers("user-agent" = "Mozilla/5.0")) %>% content() %>% "[["("chart")
   df <- lapply(a, function(date){
     lapply(date, as.data.frame) %>%
       bind_rows(.id = "causa")
