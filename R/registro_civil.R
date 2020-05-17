f702797edbc2a5ab7c992e87cb4ec1685512f92b
Freguglia <victorfreguglia@gmail.com>
2020-05-17 17:28:19 -0300

    Removed registro_civil

--- R/registro_civil.R
+++ R/registro_civil.R
@@ -1,27 +1,25 @@
-#' @name registro_civil
-#' @title Dados de https://transparencia.registrocivil.org.br/
-#' 
-#' @param state Estado no padrão `"SP"` ou `"all"` para o Brasil inteiro.
-#' @param city_id ID de cidade. Uma tabela do ID por cidade pode ser encontrada
-#' no objeto `registro_civil_cidades`.
-#' 
-#' @return Um `tibble` com as mortes por gênero, faixa etária e causa nos anos de 
-#' 2019 e 2020, do dia 16 de março até a data atual.
-#' 
-#' @importFrom dplyr bind_rows select
-#' @importFrom tidyr pivot_longer
-#' @importFrom httr GET content add_headers
-#' @importFrom glue glue
-#' @importFrom lubridate today
-#' @importFrom tibble as_tibble
-#' 
-#' @export
+# @name registro_civil
+# @title Dados de https://transparencia.registrocivil.org.br/
+# 
+# @param state Estado no padrão `"SP"` ou `"all"` para o Brasil inteiro.
+# @param city_id ID de cidade. Uma tabela do ID por cidade pode ser encontrada
+# no objeto `registro_civil_cidades`.
+# 
+# @return Um `tibble` com as mortes por gênero, faixa etária e causa nos anos de 
+# 2019 e 2020, do dia 16 de março até a data atual.
+# 
+# @importFrom dplyr bind_rows select
+# @importFrom tidyr pivot_longer
+# @importFrom httr GET content add_headers
+# @importFrom glue glue
+# @importFrom lubridate today
+# @importFrom tibble as_tibble
 registro_civil <- function(state = "all", city_id = "all"){
   if(length(city_id) > 1){stop("Um 'city_id' por vez, ou 'all' para todas as cidades to estado.")}
   if(length(state) > 1){stop("Um 'state' por vez, ou 'all' para o brasil inteiro.")}
   out <- data.frame()
   for(gender in c("M", "F")){
-  api_url <- glue::glue("https://transparencia.registrocivil.org.br/api/covid-covid-registral?start_date=2020-03-16&end_date={lubridate::today()-1}&state={state}&city_id={city_id}&chart=chart2&gender={gender}")
+  api_url <- glue::glue("https://transparencia.registrocivil.org.br/api/covid-covid-registral?start_date=2020-03-16&end_date={lubridate::today()-1}&state={state}&city_id={city_id}&places[]=HOSPITAL&places[]=DOMICILIO&places[]=VIA_PUBLICA&places[]=OUTROS&chart=chart2&gender={gender}")
   a <- GET(api_url, add_headers("user-agent" = "Mozilla/5.0")) %>% content() %>% "[["("chart")
   df <- lapply(a, function(age){
     lapply(age, as.data.frame) %>%
@@ -34,9 +32,7 @@ registro_civil <- function(state = "all", city_id = "all"){
   return(as_tibble(out))
 }
 
-#' @rdname registro_civil
-#' 
-#' @export
+# @rdname registro_civil
 registro_civil_diario <- function(state = "all", city_id = "all"){
   if(length(city_id) > 1){stop("Um 'city_id' por vez, ou 'all' para todas as cidades to estado.")}
   if(length(state) > 1){stop("Um 'state' por vez, ou 'all' para o brasil inteiro.")}
--- tests/testthat/test-registro_civil.R
+++ tests/testthat/test-registro_civil.R
@@ -1,3 +1,3 @@
-test_that("registro_civil works", {
-  expect_is(registro_civil(), "tbl_df")
-})
+#test_that("registro_civil works", {
+#  expect_is(registro_civil(), "tbl_df")
+#})
